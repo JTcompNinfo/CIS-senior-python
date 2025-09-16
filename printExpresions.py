e5b8ac51dbe54ac8b945dc6a4cee7aac8622a848
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-09-16 13:52:25 +0000

    asgore

---
+++ printExpresions.py
@@ -0,0 +1,14 @@
+'''
+Class: CIS Seniors 25-26
+Name: Joel Dinovo
+Assignment: Printing Expresions
+'''
+print("Hello, My name is Joel Dinovo")
+
+print("My Favorite song")
+print("----------------")
+print()
+print("Driving in my car right after a beer")
+print("Hey that bump is shaped like a deer")
+print("DUI? How about you die?")
+print("I'll go a hundred miles an hour")
