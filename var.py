f3ca7efe4874a73dbc2e98bccf580fb8571061e1
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-09-17 13:59:30 +0000

    nfn

---
+++ var.py
@@ -0,0 +1,20 @@
+'''
+File name: var.py
+Author: Joel Dinovo
+Date: 9/17/2025
+
+Practicing variable basics
+'''
+#Variables can save all data types
+myInt = 42
+myFloat = 3.1415
+myString = 'Joel Dinovo'
+myBool = True
+
+print("My name is: " + myString)
+
+myAge = myInt
+
+print("My age is: ", myAge)
+
+print(myFloat)
