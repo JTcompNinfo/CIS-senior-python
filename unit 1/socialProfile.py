107f2cfb1028b70986c121af190008e4e797ef17
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-09-18 14:35:24 +0000

    uncr

---
+++ unit 1/socialProfile.py
@@ -0,0 +1,31 @@
+'''
+socialProfile.py
+Joel Dinovo
+Comp Info Sci
+9/18/25
+'''
+
+username = "AppleEater27"
+bio = "Cool interersting facts."
+favorite_quote = "Study without desire spoils the memory, and it retains nothing that it takes in. ― Leonardo da Vinci"
+
+followers = 7752
+following = 5214
+post = 42
+
+average_likes_per_post = 538.7
+engagement_rate = 893.34
+
+verified_account = True 
+private_profile = False
+
+print("Username:", username)
+print("Bio:", bio)
+print("Favorite quote: ", favorite_quote)
+print("\nFollowers:", followers)
+print("Following:", following)
+print("Post number:", post)
+print("\nAverage Likes Per Post:", average_likes_per_post)
+print("Engagement Rate:", engagement_rate)
+print("\nVerified Account: ", verified_account)
+print("Private Profile:", private_profile)
