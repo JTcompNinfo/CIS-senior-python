f50ac1368c7f86c08cd644a4260283245a271121
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-09-18 13:28:28 +0000

    kcf

---
+++ dreamVacation.py
@@ -0,0 +1,20 @@
+'''
+dreamVacation.py
+Joel Dinovo
+Computer and Infromation Scince
+9/18/25
+'''
+
+location = "Iceland"
+
+cost = "1200"
+
+duration = "week"
+
+activites = "go to dimond beach and watch the sunset"
+
+companions = " my boyfreind"
+
+print("I would like to go to", location, "for vacation with " + companions + ".")
+print("I would visit for a " + duration + " and it would cost $" + cost + ".")
+print("I would", activites, "as the sunset is super pretty there.")
