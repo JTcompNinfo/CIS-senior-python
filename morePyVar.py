5c70116c327897f9c4c0ff0c24fe3d15f5ee560c
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-09-18 13:16:57 +0000

    jf

---
+++ morePyVar.py
@@ -0,0 +1,39 @@
+'''
+File name: morePyVar.py
+Author: Joel Dinovo
+Date: 9/18/2025
+
+otes about python var
+'''
+
+# Var Assigment
+firstName = "Joel"
+lastName = "Dinovo"
+gradeLevel = 12
+favSubject = "Comp Info Sci"
+age = 17
+yearsInSchool = 13
+
+print("My name is " + firstName + " " + lastName + ".")
+print("I am in ", gradeLevel, "th grade")
+print("My favorite subhect is " + favSubject)
+print("I have been in schhol for ", yearsInSchool, " years.")
+
+print("\n\n\n")
+
+print("Mathmatical Calculations:")
+
+#Storing math values
+lenght = 15
+width = 8
+height = 12
+
+#calculate area and volume and store them in variables
+area = lenght * width
+volume = lenght * width * height
+
+#print out the values
+
+print("The area of our strurcture is: ", area)
+
+print("The volume of our strurcture is: ", volume)
